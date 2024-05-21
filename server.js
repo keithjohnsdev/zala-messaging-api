66455182ca292ec20b20763eb51344a9698d5b91
keithjohnsdev <keithjohnsdev@gmail.com>
2024-05-21 10:47:14 -0400

    pushing to staging

--- server.js
+++
@@ -1,7 +0,0 @@
-const app = require('./index.js')
-
-// Start the server
-const PORT = process.env.PORT || 4000; // Use the provided port or default to 3000
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
