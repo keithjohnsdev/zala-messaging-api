ea45a3de12ebfc9feef40d28b05f3b40169db4a1
keithjohnsdev <keithjohnsdev@gmail.com>
2024-05-22 15:17:00 -0400

    setting up s3 storage for message attachments on sendMessage, analyzing file hashes to avoid unnecessary duplicates in storage

--- messageRoutes.js
+++ messageRoutes.js
@@ -1,7 +1,25 @@
 const express = require("express");
 const db = require("./db");
+const multer = require("multer");
+const { S3 } = require("aws-sdk");
+const crypto = require("crypto");
 
 const router = express.Router();
+
+// Configure multer for handling multipart/form-data
+const upload = multer();
+
+// Configure AWS S3 client
+const s3 = new S3({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION,
+});
+
+// Function to calculate file hash
+const calculateFileHash = (buffer) => {
+    return crypto.createHash("sha256").update(buffer).digest("hex");
+};
 
 router.get("/messageStatus", (req, res) => {
     const status = {
@@ -11,107 +29,169 @@ router.get("/messageStatus", (req, res) => {
     res.send(status);
 });
 
-router.post("/sendMessage", async (req, res) => {
-    const {
-        userId: senderUserId,
-        userFullName: senderFullName,
-        userEmail: senderEmail,
-    } = req;
-    const {
-        recipientFullName,
-        recipientUserId,
-        conversationId,
-        conversationTitle,
-        content,
-    } = req.body;
+router.post(
+    "/sendMessage",
+    upload.fields([{ name: "files", maxCount: 10 }]),
+    async (req, res) => {
+        const {
+            userId: senderUserId,
+            userFullName: senderFullName,
+            userEmail: senderEmail,
+        } = req;
+        const {
+            recipientFullName,
+            recipientUserId,
+            conversationId,
+            conversationTitle,
+            content,
+        } = req.body;
 
-    try {
-        // Check if the sender exists in the users table
-        let sender = await db.query(
-            "SELECT * FROM users WHERE user_uuid = $1",
-            [senderUserId]
-        );
+        console.log("-----------------------");
+        console.log("conversationTitle");
+        const attachedFiles = req.files["files"];
 
-        if (sender.rowCount === 0) {
-            // Insert the sender if they don't exist
-            console.log("sender user doesnt exist, creating new user");
-            await db.query(
-                "INSERT INTO users (user_uuid, name, email, created_at) VALUES ($1, $2, $3, NOW())",
-                [senderUserId, senderFullName, senderEmail]
-            );
-        } else if (!sender.rows[0].email) {
-            // Update the sender if email is missing
-            await db.query("UPDATE users SET email = $1 WHERE user_uuid = $2", [
-                senderEmail,
-                senderUserId,
-            ]);
-        }
-
-        // Check if the recipient exists in the users table
-        let recipient = await db.query(
-            "SELECT * FROM users WHERE user_uuid = $1",
-            [recipientUserId]
-        );
-
-        if (recipient.rowCount === 0) {
-            // Insert the recipient if they don't exist
-            console.log("recipient user doesnt exist, creating new user");
-            await db.query(
-                "INSERT INTO users (user_uuid, name, created_at) VALUES ($1, $2, NOW())",
-                [recipientUserId, recipientFullName]
-            );
-        }
-
-        let convoId = conversationId && Number(conversationId);
-
-        // Check if the conversation exists if conversationId is not provided or null
-        if (!convoId) {
-            const conversation = await db.query(
-                "SELECT * FROM conversations WHERE ((user1_uuid = $1 AND user2_uuid = $2) OR (user1_uuid = $2 AND user2_uuid = $1)) AND title = $3",
-                [senderUserId, recipientUserId, conversationTitle]
+        try {
+            // Check if the sender exists in the users table
+            let sender = await db.query(
+                "SELECT * FROM users WHERE user_uuid = $1",
+                [senderUserId]
             );
 
-            if (conversation.rowCount === 0) {
-                // Insert the conversation if it doesn't exist
-                console.log("convo doesnt exist, creating new");
-                const newConversation = await db.query(
-                    "INSERT INTO conversations (user1_uuid, user2_uuid, title, latest_message, latest_message_sender, read, length, user1_name, user2_name, created_at, updated_at) VALUES ($1, $2, $3, $4, $1, $5, $6, $7, $8, NOW(), NOW()) RETURNING conversation_id",
-                    [
-                        senderUserId,
-                        recipientUserId,
-                        conversationTitle,
-                        content,
-                        false,
-                        1,
-                        senderFullName,
-                        recipientFullName
-                    ]
+            if (sender.rowCount === 0) {
+                // Insert the sender if they don't exist
+                console.log("sender user doesn't exist, creating new user");
+                await db.query(
+                    "INSERT INTO users (user_uuid, name, email, created_at) VALUES ($1, $2, $3, NOW())",
+                    [senderUserId, senderFullName, senderEmail]
+                );
+            } else if (!sender.rows[0].email) {
+                // Update the sender if email is missing
+                await db.query(
+                    "UPDATE users SET email = $1 WHERE user_uuid = $2",
+                    [senderEmail, senderUserId]
+                );
+            }
+
+            // Check if the recipient exists in the users table
+            let recipient = await db.query(
+                "SELECT * FROM users WHERE user_uuid = $1",
+                [recipientUserId]
+            );
+
+            if (recipient.rowCount === 0) {
+                // Insert the recipient if they don't exist
+                console.log("recipient user doesn't exist, creating new user");
+                await db.query(
+                    "INSERT INTO users (user_uuid, name, created_at) VALUES ($1, $2, NOW())",
+                    [recipientUserId, recipientFullName]
+                );
+            }
+
+            let convoId = conversationId && Number(conversationId);
+
+            // Check if the conversation exists if conversationId is not provided or null
+            if (!convoId) {
+                const conversation = await db.query(
+                    "SELECT * FROM conversations WHERE ((user1_uuid = $1 AND user2_uuid = $2) OR (user1_uuid = $2 AND user2_uuid = $1)) AND title = $3",
+                    [senderUserId, recipientUserId, conversationTitle]
                 );
 
-                convoId = newConversation.rows[0].conversation_id;
+                if (conversation.rowCount === 0) {
+                    // Insert the conversation if it doesn't exist
+                    console.log("convo doesn't exist, creating new");
+                    const newConversation = await db.query(
+                        "INSERT INTO conversations (user1_uuid, user2_uuid, title, latest_message, latest_message_sender, read, length, user1_name, user2_name, created_at, updated_at) VALUES ($1, $2, $3, $4, $1, $5, $6, $7, $8, NOW(), NOW()) RETURNING conversation_id",
+                        [
+                            senderUserId,
+                            recipientUserId,
+                            conversationTitle,
+                            content,
+                            false,
+                            1,
+                            senderFullName,
+                            recipientFullName,
+                        ]
+                    );
+
+                    convoId = newConversation.rows[0].conversation_id;
+                } else {
+                    convoId = conversation.rows[0].conversation_id;
+                }
             } else {
-                convoId = conversation.rows[0].conversation_id;
+                // Update the latest_message column in the conversations table
+                await db.query(
+                    "UPDATE conversations SET latest_message = $1, latest_message_sender = $2, read = $3, updated_at = NOW(), length = length + 1 WHERE conversation_id = $4",
+                    [content, senderUserId, false, convoId]
+                );
             }
-        } else {
-            // Update the latest_message column in the conversations table
-            await db.query(
-                "UPDATE conversations SET latest_message = $1, latest_message_sender = $2, read = $3, updated_at = NOW(), length = length + 1 WHERE conversation_id = $4",
-                [content, senderUserId, false, convoId]
-            );
+
+            // Insert the message
+            const messageQuery = `
+                INSERT INTO messages (conversation_id, sender_uuid, recipient_uuid, content, timestamp) 
+                VALUES ($1, $2, $3, $4, NOW()) RETURNING message_id
+            `;
+            const messageResult = await db.query(messageQuery, [
+                convoId,
+                senderUserId,
+                recipientUserId,
+                content,
+            ]);
+
+            const messageId = messageResult.rows[0].message_id;
+
+            // Insert the file information
+            if (attachedFiles && attachedFiles.length > 0) {
+                for (const file of attachedFiles) {
+                    const fileHash = calculateFileHash(file.buffer);
+
+                    // Check if the file already exists in the database
+                    const fileCheckQuery =
+                        "SELECT * FROM files WHERE hash = $1";
+                    const existingFile = await db.query(fileCheckQuery, [
+                        fileHash,
+                    ]);
+
+                    let filePath;
+
+                    if (existingFile.rowCount > 0) {
+                        filePath = existingFile.rows[0].file_path;
+                    } else {
+                        // Upload new file to S3
+                        const uploadParams = {
+                            Bucket: process.env.S3_BUCKET_NAME,
+                            Key: `${fileHash}-${file.originalname}`,
+                            Body: file.buffer,
+                            ContentType: file.mimetype,
+                        };
+                        const s3Response = await s3
+                            .upload(uploadParams)
+                            .promise();
+                        filePath = s3Response.Location;
+
+                        // Insert file metadata into the database
+                        await db.query(
+                            "INSERT INTO files (hash, file_path, file_name, created_at) VALUES ($1, $2, $3, NOW())",
+                            [fileHash, filePath, file.originalname]
+                        );
+                    }
+
+                    // Associate file with the message
+                    await db.query(
+                        "INSERT INTO message_files (message_id, file_path, file_name, created_at) VALUES ($1, $2, $3, NOW())",
+                        [messageId, filePath, file.originalname]
+                    );
+                }
+            }
+
+            res.status(201).json({
+                message: "Message sent successfully with files",
+            });
+        } catch (error) {
+            console.error("Error sending message:", error);
+            res.status(500).json({ error: "Internal Server Error" });
         }
-
-        // Insert the message
-        await db.query(
-            "INSERT INTO messages (conversation_id, sender_uuid, recipient_uuid, content, timestamp) VALUES ($1, $2, $3, $4, NOW())",
-            [convoId, senderUserId, recipientUserId, content]
-        );
-
-        res.status(201).json({ message: "Message sent successfully" });
-    } catch (error) {
-        console.error("Error sending message:", error);
-        res.status(500).json({ error: "Internal Server Error" });
     }
-});
+);
 
 // read message
 router.post("/readMessage", async (req, res) => {
