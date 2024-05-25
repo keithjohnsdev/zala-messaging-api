bbf474d5253fd8a341162e8998555bfcc4f5bdf8
keithjohnsdev <keithjohnsdev@gmail.com>
2024-05-24 22:29:37 -0400

    adjusted sendMessage to do gql query for profile pic urls and store on conversations table for performance boost on client side 4

--- messageRoutes.js
+++ messageRoutes.js
@@ -4,8 +4,6 @@ const multer = require("multer");
 const { S3 } = require("aws-sdk");
 const crypto = require("crypto");
 
-const { GetUserAttachments } = require("./helperFunctions");
-
 const router = express.Router();
 
 // Configure multer for handling multipart/form-data
@@ -258,5 +256,41 @@ router.get("/sent/:userId", async (req, res) => {
         res.status(500).json({ error: "Internal Server Error" });
     }
 });
+
+
+
+
+//----- Helper Functions
+
+async function GetUserAttachments(userId) {
+    let response = await axios.post(
+        "https://zala-stg.herokuapp.com/gql",
+        {
+            query: `
+            query GetUserAttachments($userId: ID!) {
+                user(id: $userId) {
+                    attachments(labels: ["profile_picture"]) {
+                        id
+                        label
+                        contentUrl
+                    }
+                }
+            }
+            `,
+            variables: {
+                userId: userId, // Pass the userId variable here
+            },
+        },
+        {
+            headers: {
+                Authorization: token,
+                "Content-Type": "application/json",
+            },
+        }
+    );
+
+    // If the request is successful, assign the response data to user1ProfilePic
+    return response.data.data.user.attachments;
+}
 
 module.exports = router;
