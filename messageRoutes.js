22c0927771058bdc462d64e6714538432ef63a8b
Keith Johns <keithjohnsdev@gmail.com>
2024-06-09 12:56:35 -0400

    resetting read_by array on conversation update

--- messageRoutes.js
+++ messageRoutes.js
@@ -536,15 +536,15 @@ router.post(
                         "Conversation does not exist, creating new conversation"
                     );
                     const newConversation = await db.query(
-                        "INSERT INTO conversations (users, title, latest_message, latest_message_sender, read, length, sorted_uuids, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, $7, NOW(), NOW()) RETURNING conversation_id",
+                        "INSERT INTO conversations (users, title, latest_message, latest_message_sender, length, sorted_uuids, read_by, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, $7, NOW(), NOW()) RETURNING conversation_id",
                         [
                             usersArray,
                             conversationTitle,
                             messageBodyStrippedHTML,
                             senderUserId,
-                            false,
                             1,
                             sortedIds,
+                            []
                         ]
                     );
 
